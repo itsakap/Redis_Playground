dfb9ef25c5f61883fd367f99dc58ea3af647d5ae
AdamHKaplan <AdamHKaplan@gmail.com>
2014-08-18 14:41:30 -0700

    huh?

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  validates_presence_of :name
+  def follow!(user)
+    $redis.multi do
+      $redis.sadd(self.redis_key(:following), user.id)
+      $redis.sadd(user.redis_key(:followed),self.id)
+    end
+  end
+  def following
+    user_ids = $redis.smembers(self.redis_key(:following))
+    User.where(:id => user_ids)
+  end
+  def followed
+    user_ids = $redis.smembers(self.redis_key(:followed))
+    User.where(:id => user_ids)
+  end
+  def redis_key(str)
+    "user:#{self.id}:#{str}"
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe User do
+  before :each do
+    @valid_attributes = {
+      id:1,
+      name: "Bob"
+    }
+    @valid_attributes_2 = {
+      id:2,
+      name: "Jeff"
+    }
+  end
+  context 'when all attributes are valid' do
+    it 'the record is valid' do
+        expect(User.new(@valid_attributes)).to be_valid
+    end
+    context 'name is missing' do
+      it 'the record is not valid' do
+        expect(User.new(name:'')).to be_invalid
+      end
+    end
+  end
+  context 'when user 1 follows user 2' do
+    before :each do
+      @user_1 = User.create!(@valid_attributes)
+      @user_2 = User.create!(@valid_attributes_2)
+      @user_1.follow!(@user_2)
+    end
+    it "should add 2nd user to list of users 1st user is following" do
+      expect(@user_1.following).to include(@user_2)
+    end
+    it "should add 1st user to list of 2nd user's followers" do
+      expect(@user_2.followed).to include(@user_1)
+    end
+  end
+end
